4ca145fd669cba7df8835f1ef27af48d2060516f
Abhishek Raj <abhishek@betatestsolutions.com>
2019-09-17 17:19:04 +0530

    added and configured firebase to app

---
+++ config/firebase-config.js
@@ -0,0 +1,14 @@
+(function () {
+    const firebaseConfig = {
+        apiKey: "AIzaSyCjRV0ds57xbcehsJTIcWsipxV6jDnxcII",
+        authDomain: "betatest-presentation.firebaseapp.com",
+        databaseURL: "https://betatest-presentation.firebaseio.com",
+        projectId: "betatest-presentation",
+        storageBucket: "",
+        messagingSenderId: "438122853060",
+        appId: "1:438122853060:web:72f85d4e9ae099b36e8f91"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+
+}());
