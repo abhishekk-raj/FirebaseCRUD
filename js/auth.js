6fd63a9fee1a096a1a1759364e8e23da140ff73a
Abhishek Raj <abhishek@betatestsolutions.com>
2019-09-18 01:47:47 +0530

    login validation and toast added to show user appropriate message

--- js/auth.js
+++ js/auth.js
@@ -7,11 +7,29 @@ function login() {
     const loginForm = $('#loginForm').serializeArray();
     firebase.auth().signInWithEmailAndPassword(loginForm[0].value, loginForm[1].value)
         .then(() => {
-            navigateToProductsList();
+            console.log('Login Successful');
+            showLoginToast('LogIn Successful', 'success');
+
+            setTimeout(() => {
+                navigateToProductsList();
+            }, 500);
         })
         .catch(err => {
             console.log('Error while SignIn :: ', err.message);
+            showLoginToast(err.message, 'failed');
         });
+}
+
+/**
+ * Show toast to user whether login success/fail
+ */
+function showLoginToast(toastMessage, messageType) {
+    if (messageType === 'success') {
+        $('#loginToastMessage').text(toastMessage).addClass('toast-success');
+    } else {
+        $('#loginToastMessage').text(toastMessage).addClass('toast-failed');
+    }
+    $('#loginToast').toast('show');
 }
 
 function navigateToProductsList() {
